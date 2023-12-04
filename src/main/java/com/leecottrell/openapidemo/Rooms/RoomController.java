512364ed8e2723683737545739139ab2b92248d8
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2023-12-04 14:04:58 -0500

    update

--- src/main/java/com/leecottrell/openapidemo/Rooms/RoomController.java
+++ src/main/java/com/leecottrell/openapidemo/Rooms/RoomController.java
@@ -4,8 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.util.*;
 
 @RestController
